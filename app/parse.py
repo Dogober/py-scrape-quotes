20a4e0bf62db1fa2abd6cfa28ece60fdcbb9466a
dogober <dogoberrr@gmail.com>
2025-03-27 21:36:43 +0200

    Solution

--- app/parse.py
+++ app/parse.py
@@ -1,4 +1,10 @@
-from dataclasses import dataclass
+import csv
+from dataclasses import dataclass, fields, astuple
+import requests
+
+from bs4 import BeautifulSoup, Tag
+
+BASE_URL = "https://quotes.toscrape.com/"
 
 
 @dataclass
@@ -8,8 +14,40 @@ class Quote:
     tags: list[str]
 
 
+QUOTE_FIELDS = [field.name for field in fields(Quote)]
+
+
+def parse_single_quote(quote: Tag) -> Quote:
+    return Quote (
+        text=quote.select_one(".text").text,
+        author=quote.select_one(".author").text,
+        tags=[tag.text for tag in quote.find_all(name="a", attrs={"class": "tag"})]
+    )
+
+
+def parse_quotes() -> [Quote]:
+    current_page = 1
+    result = []
+
+    while True:
+        content = requests.get(f"{BASE_URL}page/{current_page}/").content
+        soup = BeautifulSoup(content, "html.parser")
+        quotes = soup.select(".quote")
+
+        if not quotes:
+            break
+
+        result.extend([parse_single_quote(quote) for quote in quotes])
+        current_page += 1
+
+    return result
+
+
 def main(output_csv_path: str) -> None:
-    pass
+    with open(output_csv_path, "w", encoding="utf-8", newline="") as source_file:
+        writer = csv.writer(source_file)
+        writer.writerow(QUOTE_FIELDS)
+        writer.writerows([astuple(quote) for quote in parse_quotes()])
 
 
 if __name__ == "__main__":
